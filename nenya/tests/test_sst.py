79d0ed39a9bbff7ee92ebdde6f412fcc45d9fe1a
profxj <xavier@ucolick.org>
2025-05-20 10:11:13 -0700

    debug

--- nenya/tests/test_sst.py
+++ nenya/tests/test_sst.py
@@ -5,5 +5,6 @@ import numpy as np
 
 from nenya.train import main as train_main
 
-# s3 get s3://viirs/Tables/VIIRS_2013_tst_train_98.parquet
-train_main("opts_nenya_viirs_test.json", debug=True)
+# aws3 cp s3://viirs/Tables/VIIRS_2013_tst_train_98.parquet .
+# aws3 cp s3://viirs/PreProc/VIIRS_2013_98clear_192x192_preproc_viirs_tst_train.h5 .
+train_main("opts_nenya_viirs_test.json", debug=False)

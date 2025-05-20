e67af9df1db43116cde526b18c09c8660b9d71d8
profxj <xavier@ucolick.org>
2025-05-20 13:45:19 -0700

    cooking with gas

--- papers/InformationContent/Analysis/py/nenya_swot_ssr.py
+++ papers/InformationContent/Analysis/py/nenya_swot_ssr.py
@@ -9,6 +9,9 @@ from nenya.train import main as train_main
 # aws s3 cp s3://odsl/nasa_oceanai_workshop2025/justin/swot_L2unsmoothed_1dayRepeat_ssr_images_unh/Pass_003.h5 . --profile nasa-oceanai
 
 # cp ~/Oceanography/python/nenya/papers/InformationContent/Analysis/opts_nenya_swot_test.json .
+# cp ~/Oceanography/python/nenya/papers/InformationContent/Analysis/opts_nenya_swot_fast.json .
+
+# aws s3 sync models s3://odsl/nasa_oceanai_workshop2025/justin/swot_L2unsmoothed_1dayRepeat_ssr_images_unh/ --profile nasa-oceanai 
 
 def main(flg):
 
@@ -20,6 +23,7 @@ def main(flg):
     # Main
     if flg == 2:
         train_main("opts_nenya_swot_fast.json", debug=False)
+
     #train_main("opts_nenya_viirs.json", debug=False)
 
 # Command line execution

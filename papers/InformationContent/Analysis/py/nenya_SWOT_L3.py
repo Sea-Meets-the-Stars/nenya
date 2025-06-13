2dd4461d3343f10a95aea7acfdf0780a5aefd95a
profxj <xavier@ucolick.org>
2025-06-13 11:15:22 -0700

    swot

---
+++ papers/InformationContent/Analysis/py/nenya_SWOT_L3.py
@@ -0,0 +1,23 @@
+""" Run Nenya on the ImageNet dataset """
+
+from nenya import workflow
+import info_defs
+
+from IPython import embed
+
+def main(task:str):
+    opts_file, mnist_path, preproc_file, latents_file = info_defs.grab_paths('SWOT_L3')
+    if task == 'train':
+        workflow.train(opts_file, debug=False)
+    elif task == 'evaluate':
+        workflow.evaluate(opts_file, preproc_file, local_model_path=mnist_path,
+                          latents_file=latents_file) 
+    elif task == 'chk_latents':
+        workflow.chk_latents('SWOT_L3', latents_file, preproc_file, 100)
+    else:
+        raise ValueError(f"Unknown task: {task}")
+
+# Command line execution
+if __name__ == '__main__':
+    task = workflow.return_task()
+    main(task)
